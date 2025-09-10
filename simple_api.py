5551e567886a7c66045e92009801ec20ef3b8ce1
nahye <knh8783@gmail.com>
2025-09-10 20:13:36 +0900

    feat: google login 성공

--- simple_api.py
+++ simple_api.py
@@ -43,12 +43,19 @@ app = FastAPI(
 )
 
 # CORS 미들웨어 설정
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origins=["http://localhost:3000"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 app.include_router(auth_router)
